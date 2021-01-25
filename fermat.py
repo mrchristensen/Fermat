4a976591e72c336a6581e7f6df46ed24e07b7a73
Matt <mr.christensen@gmail.com>
2021-01-25 12:31:35 -0700

    Added clamps to ranges of number genorated and time complexity comments

--- fermat.py
+++ fermat.py
@@ -2,59 +2,68 @@ import random
 
 
 def prime_test(N, k):
-    # This is main function, that is connected to the Test button. You don't need to touch it.
     return fermat(N, k), miller_rabin(N, k)
 
 
-def mod_exp(x, y, N):
-    if y == 0:
+# Algorithm from Figure 1.4
+def mod_exp(x, y, N):  # Time Complexity: O(n(n^2+n^2)) = O(n^3)
+    if y == 0:  # Time Complexity: Runs y times, let it be n
         return 1
 
-    z = mod_exp(x, y // 2, N)
+    z = mod_exp(x, y // 2, N)  # Time Complexity: division = O(n^2)
     if y % 2 == 0:  # If y is even
-        return (z ** 2) % N
+        return (z ** 2) % N  #
     else:  # If y is odd
-        return (x * (z ** 2)) % N
+        return (x * (z ** 2)) % N  # Time Complexity: mult and exp = O(n^2 + n^2) = O(n^2)
 
 
+# From pg todo
 def fprobability(k):
-    return 1 / 2 ** k
+    return 1 - (1 / 2) ** k  # Time Complexity: Divide and power = O(n^2 + n^2) = O(n^2)
 
 
+# From pg 28
 def mprobability(k):
-    return (3 / 4) ** k  # todo: check to see if this is right
+    return 1 - (1 / 4) ** k  # Time Complexity: Divide and power = O(n^2 + n^2) = O(n^2)
 
 
-def fermat(N, k):
-    randomNums = random.sample(range(1, N - 1),
-                               k)  # todo: figure out if we need to handle when the user want to sample more than is possible (with mathfclamp or something)
+# Algorithm from Figure 1.8 (pg 27)
+def fermat(N, k):  # Time Complexity: O(k(n^3)) = O(n^3)
+    if N == 1:
+        return "composite"
 
-    for a in randomNums:
-        if mod_exp(a, N - 1, N) != 1:
+    randomNums = random.sample(range(1, N), min(k, N - 1))  # Generate k random nums (with no repeats) where 0 < a < n and is unique
+
+    # Time Complexity: runs k times
+    for a in randomNums:  # Iter through a1, a2, ... , ak
+        if mod_exp(a, N - 1, N) != 1:  # Time Complexity: O(n^3)
             return "composite"
 
     return 'prime'
 
 
-def miller_rabin(N, k):
-    randomNums = random.sample(range(1, N - 1),
-                               k)  # todo: figure out if we need to handle when the user want to sample more than is possible (with mathfclamp or something)
+def miller_rabin(N, k): # Time Complexity: O(k(n^3 + n(n^2))) = O(n^3)
+    randomNums = random.sample(range(1, N), min(k, N - 1))  # todo: check this
 
+    # Time Complexity: runs k times
     for a in randomNums:
 
-        res = mod_exp(a, N - 1, N)  # Equivalent to the Fermat Test
+        # Equivalent to the Fermat Test
+        res = mod_exp(a, N - 1, N)  # Time Complexity: O(n^3)
         if res != 1:
             return "composite"
 
         listofSqrt = []
         n = (N - 1)
-        while (n % 2 == 0):  # Square root until you get to an odd exponent
+        # Square root until you get to an odd exponent
+        while (n % 2 == 0):  # Time Complexity: runs (1/2)n times
             listofSqrt.append(mod_exp(a, n / 2, N))
-            n = n // 2
+            n = n // 2  # Time Complexity: division = O(n^2)
 
-        for sqrt in listofSqrt:  # Go through list of squares
+        # Go through list of squares
+        for sqrt in listofSqrt:  # Time Complexity: run todo times
             if sqrt != 1:  # If it's not 1
-                if sqrt == N - 1:
+                if sqrt == N - 1:  # subtraction = O(1)
                     break
                 else:  # And if it's not N-1 (or -1 % N - they are equivalent)
                     return "composite"  # Than it's composite
